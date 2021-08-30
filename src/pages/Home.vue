afeabf17322cc2dc59126428703c5ee2fc2947aa
dell_counterservice <thaninpat.o@gmail.com>
2021-08-30 17:11:15 +0700

    Handle skipWaiting v3.0.5.2

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <base-layout pageTitle="(PWA)v3.0.5">
+  <base-layout pageTitle="(PWA)v3.0.5.1">
     <v-snackbar bottom right :value="updateExists" :timeout="0" color="primary">
       An update is available
       <v-btn text @click="refreshApp">
