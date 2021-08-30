e379fa86007fdff9770b6547f23e009c4729dcd9
dell_counterservice <thaninpat.o@gmail.com>
2021-08-30 16:02:02 +0700

    Add page InvoiceStatement

---
+++ src/components/Statement.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mb-5">
+    <v-row class="my-2">
+      <v-col cols="12" md="12">
+        <div>
+          <strong>Statement</strong>
+        </div>
+      </v-col>
+    </v-row>
+    <table>
+      <tr class="item-list">
+        <th>No</th>
+        <th>Statement</th>
+        <th>File Name</th>
+        <th>Download</th>
+      </tr>
+      <tr class="item-list-detail" v-for="item in items" :key="item.id">
+        <td>{{ item.id }}</td>
+        <td>{{ item.statementDate }}</td>
+        <td>{{ item.fileName }}</td>
+        <td>
+          <a href="#">Download</a>
+        </td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['items'],
+}
+</script>
--- src/pages/InvoiceStatementTax.vue
+++ src/pages/InvoiceStatementTax.vue
@@ -1,9 +1,76 @@
 <template>
-  <base-layout pageTitle="Invoice Statement Tax"> </base-layout>
+  <base-layout pageTitle="Statement/Tax Invoice">
+    <v-container>
+      <statement :items="items" />
+      <v-divider></v-divider>
+      <tax-invoice :items="items" />
+    </v-container>
+  </base-layout>
 </template>
 
 <script>
-export default {}
+import Statement from '../components/Statement.vue'
+import TaxInvoice from '../components/TaxInvoice.vue'
+export default {
+  components: { Statement, TaxInvoice },
+  data() {
+    return {
+      items: [
+        {
+          id: '1',
+          statementDate: '2021-02-02',
+          fileName: 'abc_1234_aasdf_1212121.pdf',
+          download: true,
+        },
+        {
+          id: '2',
+          statementDate: '2021-02-02',
+          fileName: 'abc1_asas_234.pdf',
+          download: true,
+        },
+        {
+          id: '3',
+          statementDate: '2021-02-02',
+          fileName: 'abc1_aslk_l234.pdf',
+          download: true,
+        },
+        {
+          id: '4',
+          statementDate: '2021-02-02',
+          fileName: 'abc1234_as3456.pdf',
+          download: true,
+        },
+        {
+          id: '5',
+          statementDate: '2021-02-02',
+          fileName: 'abc1234_as3456.pdf',
+          download: true,
+        },
+      ],
+    }
+  },
+}
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scope>
+table {
+  width: 100%;
+}
+tr {
+  background-color: hsl(0, 0%, 98%);
+}
+tr:nth-child(odd) {
+  background-color: hsl(110, 100%, 95%);
+}
+th {
+  padding: 0px 4px;
+  font-weight: bold;
+  background-color: hsl(0, 0, 90%);
+}
+td {
+  text-align: center;
+}
+a {
+  text-decoration-line: none;
+}
+</style>
